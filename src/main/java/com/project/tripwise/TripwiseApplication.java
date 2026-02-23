b19f1bad2e6d82e45e93169b55afd0be5689fe4f
sally <wykee001@gmail.com>
2026-02-23 17:28:34 -0500

    first commit

---
+++ src/main/java/com/project/tripwise/TripwiseApplication.java
@@ -0,0 +1,13 @@
+package com.project.tripwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tripwise/TripwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tripwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
