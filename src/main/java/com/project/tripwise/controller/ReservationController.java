6b64b147d7bfeb924daf66f2ab8a301fd13360a6
sally <wykee001@gmail.com>
2026-03-11 17:18:21 -0400

    feat: Ensure reservation download URLs are absolute and remove unused imports.

--- src/main/java/com/project/tripwise/controller/ReservationController.java
+++ src/main/java/com/project/tripwise/controller/ReservationController.java
@@ -1,7 +1,5 @@
 package com.project.tripwise.controller;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
